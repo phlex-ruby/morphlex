0c6514f75be632a87766f9bdda6de313e6bd1871
Joel Drapper <joel@drapper.me>
2024-03-01 09:52:02 +0000

    Update morphlex.test.js

--- test/morphlex.test.js
+++ test/morphlex.test.js
@@ -2,7 +2,7 @@ import { fixture, html, expect } from "@open-wc/testing";
 import { morph } from "../";
 
 describe("morph", () => {
-	it.only("doesn't cause iframes to reload", async () => {
+	it("doesn't cause iframes to reload", async () => {
 		const original = await fixture(
 			`<div>
 				<h1></h1>
@@ -17,45 +17,68 @@ describe("morph", () => {
 			</div>`,
 		);
 
-		const originalIframe = original.querySelector("iframe");
 		morph(original, reference);
 		expect(original.outerHTML).to.equal(reference.outerHTML);
 	});
 
-	it("supports nodes from iframes", async () => {
-		const iframe = await fixture(html`<iframe></iframe>`);
+	it("can stream html", async () => {
+		const iframe = await fixture(`<iframe></iframe>`);
 
-		const original = await fixture(html`<h1>Hello World</h1>`);
-		const eventual = iframe.contentDocument.createElement("h1");
+		const contentDocument = iframe.contentDocument;
 
-		eventual.textContent = "Hello Joel";
+		const observer = new MutationObserver((mutations) => {
+			mutations.forEach((mutation) => {
+				console.log(mutation.target, mutation.addedNodes);
+			});
+		});
 
-		iframe.contentDocument.body.appendChild(eventual);
+		contentDocument.write("<body>");
 
-		morph(original, eventual);
+		const body = contentDocument.querySelector("body");
 
-		expect(original.textContent).to.equal("Hello Joel");
+		observer.observe(body, { childList: true, subtree: true });
+
+		contentDocument.write("<h1>Hello World</h1>");
+		contentDocument.write("<p>Wh");
+		contentDocument.write("at's up?</p>");
+		contentDocument.write("<div><span>Hello</span><span>Another span");
+		contentDocument.write("");
 	});
 
-	it("syncs text content", async () => {
-		const a = await fixture(html`<h1></h1>`);
-		const b = await fixture(html`<h1>Hello</h1>`);
+	// it("supports nodes from iframes", async () => {
+	// 	const iframe = await fixture(html`< iframe ></iframe > `);
 
-		new MutationObserver(() => {
-			throw new Error("The to node was mutated.");
-		}).observe(b, { attributes: true, childList: true, subtree: true });
+	// 	const original = await fixture(html`< h1 > Hello World</h1 > `);
+	// 	const eventual = iframe.contentDocument.createElement("h1");
 
-		morph(a, b);
+	// 	eventual.textContent = "Hello Joel";
 
-		expect(a.textContent).to.equal(b.textContent);
-	});
+	// 	iframe.contentDocument.body.appendChild(eventual);
 
-	it("removes excess elements", async () => {
-		const a = await fixture(html`<h1><div></div></h1>`);
-		const b = await fixture(html`<h1></h1>`);
+	// 	morph(original, eventual);
 
-		morph(a, b);
+	// 	expect(original.textContent).to.equal("Hello Joel");
+	// });
 
-		expect(a.outerHTML).to.equal(b.outerHTML);
-	});
+	// it("syncs text content", async () => {
+	// 	const a = await fixture(html`< h1 ></h1 > `);
+	// 	const b = await fixture(html`< h1 > Hello</h1 > `);
+
+	// 	new MutationObserver(() => {
+	// 		throw new Error("The to node was mutated.");
+	// 	}).observe(b, { attributes: true, childList: true, subtree: true });
+
+	// 	morph(a, b);
+
+	// 	expect(a.textContent).to.equal(b.textContent);
+	// });
+
+	// it("removes excess elements", async () => {
+	// 	const a = await fixture(html`< h1 > <div></div></h1 > `);
+	// 	const b = await fixture(html`< h1 ></h1 > `);
+
+	// 	morph(a, b);
+
+	// 	expect(a.outerHTML).to.equal(b.outerHTML);
+	// });
 });
